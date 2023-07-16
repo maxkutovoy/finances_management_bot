433d55fed6d7a0624676103a9796f9d00f5cf409
Max Kutovoy <kutovoymail@gmail.com>
2023-07-15 21:31:03 -0500

    Made the launch of the bot with the django service command.

--- finances_management/accounting/management/commands/bot.py
+++ finances_management/accounting/management/commands/bot.py
@@ -1,4 +1,6 @@
 from environs import Env
+from django.conf import settings
+from django.core.management.base import BaseCommand
 from telegram import ForceReply, InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
     Application,
@@ -19,22 +21,36 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         reply_markup=ForceReply(selective=True),
     )
 
+
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
     await update.message.reply_text(update.message.text)
 
-def main() -> None:
-    env = Env()
-    env.read_env()
 
-    telegram_token = env.str('TG_TOKEN')
-    application = Application.builder().token(telegram_token).build()
+class Command(BaseCommand):
+    help = "Telegram bot for finances management"
 
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    def handle(self, *args, **options):
+        telegram_token = settings.TG_TOKEN
+        application = Application.builder().token(telegram_token).build()
 
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
+        application.add_handler(CommandHandler("start", start))
+        application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+        application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
-if __name__ == "__main__":
-    main()
+
+#
+#
+
+#
+#
+# def main() -> None:
+#
+#
+#
+#
+#
+# if __name__ == "__main__":
+#     main()
