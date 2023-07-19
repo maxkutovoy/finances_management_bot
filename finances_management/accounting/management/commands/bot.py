4d44027c2ddf82549abb5a7f91b9eea3b0223616
Max Kutovoy <kutovoymail@gmail.com>
2023-07-19 14:23:20 -0500

    Create base django models.

--- finances_management/accounting/management/commands/bot.py
+++ finances_management/accounting/management/commands/bot.py
@@ -16,9 +16,10 @@ from telegram.ext import (
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user
+    print(update.message)
     await update.message.reply_html(
         rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
+        reply_markup=ForceReply(selective=True)
     )
 
 
--- finances_management/accounting/models.py
+++ finances_management/accounting/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.db.models import Sum, F, Prefetch
 
-# Create your models here.
+
+class User(models.Model):
+    telegram_id = models.PositiveIntegerField(
+        verbose_name='Telegram ID',
+        unique=True,
+        db_index=True,
+    ),
+
+    class Meta:
+        verbose_name = 'User ID'
+
+
+class Wallet(models.Model):
+    user = models.ForeignKey(
+        'accounting.User',
+        verbose_name='User ID',
+        related_name='wallets',
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(
+        verbose_name='Wallet name',
+        max_length=60,
+    )
+    balance = models.DecimalField(
+        verbose_name='Current balance',
+        max_digits=9,
+        decimal_places=2,
+    )
+
+    class Meta:
+        verbose_name = 'Wallet'
+
+
+class Transaction(models.Model):
+    transaction_types = [
+        ('in', 'income'),
+        ('ex', 'expanse')
+    ]
+
+    wallet = models.ForeignKey(
+        'accounting.Wallet',
+        verbose_name='wallet',
+        related_name='transactions',
+        on_delete=models.CASCADE,
+    )
+    value = models.DecimalField(
+        verbose_name='Amount',
+        max_digits=9,
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+    )
+    description = models.TextField(
+        verbose_name='Description',
+        max_length=1000,
+        blank=True,
+
+    )
+    date = models.DateTimeField(
+        verbose_name='Transaction date',
+        auto_now_add=True,
+    )
+    type_of_transaction = models.CharField(max_length=60, choices=transaction_types)
+
+    class Meta:
+        verbose_name = 'Income'
